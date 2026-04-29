56123b37f65cbbe5f391d2c13010a3627a35d25b
maharanidigitalhub-dev <maharanidigitalhub@gmail.com>
2026-04-30 06:13:11 +0800

    Configure remote image patterns in next.config.js
    Added remote image patterns for Supabase.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
+    ],
   },
+};
 
   async redirects() {
     return [
